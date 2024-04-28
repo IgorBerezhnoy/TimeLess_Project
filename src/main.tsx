8a36a002007d01e5a150a7a326c68c1f8cadf4a8
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2024-04-28 14:11:35 +0800

    [feat]: added fonts

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { App } from '@/App'
 import ReactDOM from 'react-dom/client'
 
 import './styles/index.scss'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/600.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
